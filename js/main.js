117e13f5476fd1857466c6596ddccb69d3313e17
Igor Seleznov and Justin Carothers <igor@>
2016-06-02 10:41:02 -0700

    created a website to check out your favorite animals

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
-
-
-
 $(document).ready(function() {
 
-  var age = parseInt(prompt("How old arte you"));
+  $(".col-sm-4").click(function(){
 
-  if (age >= 18) {
-    $("#intro").hide();
-    $('#eighteen').show();
-  } else {
-    $('#notEighteen').show();
-  };
+   var choice = $(this).attr('id'); // or var clickedBtnID = this.id
+   if (choice === 'turtle') {
+     $("#main").hide();
+     $(".turtlePage").show();
+   }
+   else if (choice === 'snake') {
+     $("#main").hide();
+     $(".snakePage").show();
+   }
+   else if (choice === 'insect')
+   {
+     $("#main").hide();
+     $(".insectPage").show();
+   }
+   else {
+     return;
+   }
+ });
 });
-  // $(".NO").click(function(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+// $(document).ready(function() {
+//
+//   var age = parseInt(prompt("How old arte you"));
+//
+//   if (age >= 18) {
+//     $("#intro").hide();
+//     $('#eighteen').show();
+//   } else {
+//     $('#notEighteen').show();
+//   };
+// });
+//   // $(".NO").click(function(){
